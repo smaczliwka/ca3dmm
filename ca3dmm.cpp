3af6e920dc3aec0553d10d11c4e8c370eff4c4b8
Karolina Drabik <kd417818@students.mimuw.edu.pl>
2023-06-08 17:09:21 +0200

    Print

--- ca3dmm.cpp
+++ ca3dmm.cpp
@@ -174,6 +174,10 @@ int main(int argc, char *argv[]) {
         int dim_m = row < m % Pm ? (m + Pm - 1) / Pm : m / Pm;
         int dim_n = col < n % Pn ? (n + Pn - 1) / Pn : n / Pn;
 
+        int k_ceil = (k + Pk - 1) / Pk;
+        int m_ceil = (m + Pm - 1) / Pm;
+        int n_ceil = (n + Pn - 1) / Pn;
+
         int offset_k = slice * (k / Pk) + std::min(slice, k % Pk);
         int offset_m = row * (m / Pm) + std::min(row, m % Pm);
         int offset_n = col * (n / Pn) + std::min(col, n % Pn);
@@ -189,12 +193,12 @@ int main(int argc, char *argv[]) {
         //     std::cout << "(" << row << ", " << slice << ")\n";
         // }
 
-        std::vector<double> A(dim_k * dim_m, 0);
+        std::vector<double> A(k_ceil * m_ceil, 0);
         
         if (col == 0) { // Generuję kawałek macierzy A
             for (int i = 0; i < dim_m; i++) {
                 for (int j = 0; j < dim_k; j++) {  
-                    A[i * dim_k + j] = generate_double(sA, i + offset_m, j + offset_k);
+                    A[i * k_ceil + j] = generate_double(sA, i + offset_m, j + offset_k);
                     //std::cout << A[i * dim_k + j] << ", ";
                 }
                 //std::cout << "\n";
@@ -204,7 +208,7 @@ int main(int argc, char *argv[]) {
         MPI_Bcast(
             &A[0],  /* the message will be written here */
                     /* if my_rank==root, the message will be read from here */
-            dim_m * dim_k,  /* number of items in the message */
+            m_ceil * k_ceil,  /* number of items in the message */
             MPI_DOUBLE, /* type of data in the message */
             0,   /* if my_rank==root, I'm sending, otherwise I'm receiving */
             row_slice_comm  /* communicator to use */
@@ -213,7 +217,7 @@ int main(int argc, char *argv[]) {
         // if (col == Pn - 1) {
         //     for (int i = 0; i < dim_m; i++) {
         //         for (int j = 0; j < dim_k; j++) {  
-        //             std::cout << A[i * dim_k + j] << ", ";
+        //             std::cout << A[i * k_ceil + j] << ", ";
         //         }
         //         std::cout << "\n";
         //     }
@@ -222,12 +226,12 @@ int main(int argc, char *argv[]) {
         MPI_Comm col_slice_comm; // ten sam col w tym samym slice
         MPI_Comm_split(active, col * Pk + slice, row, &col_slice_comm);
 
-        std::vector<double> B(dim_k * dim_n, 0);
+        std::vector<double> B(k_ceil * n_ceil, 0);
 
         if (row == 0) { // Generuję kawałek macierzy B
             for (int i = 0; i < dim_n; i++) {
                 for (int j = 0; j < dim_k; j++) {
-                    B[i * dim_k + j] = generate_double(sB, j + offset_k, i + offset_n);
+                    B[i * k_ceil + j] = generate_double(sB, j + offset_k, i + offset_n);
                     // std::cout << B[i * dim_k + j] << " ";
                 }
             }
@@ -236,7 +240,7 @@ int main(int argc, char *argv[]) {
         MPI_Bcast(
             &B[0],  /* the message will be written here */
                     /* if my_rank==root, the message will be read from here */
-            dim_n * dim_k,  /* number of items in the message */
+            n_ceil * k_ceil,  /* number of items in the message */
             MPI_DOUBLE, /* type of data in the message */
             0,   /* if my_rank==root, I'm sending, otherwise I'm receiving */
             col_slice_comm  /* communicator to use */
@@ -245,17 +249,17 @@ int main(int argc, char *argv[]) {
         // if (row == Pm - 1) {
         //     for (int i = 0; i < dim_n; i++) {
         //         for (int j = 0; j < dim_k; j++) {  
-        //             std::cout << B[i * dim_k + j] << ", ";
+        //             std::cout << B[i * k_ceil + j] << ", ";
         //         }
         //     }
         // }        
 
-        std::vector<double> C(dim_m * dim_n, 0);
+        std::vector<double> C(m_ceil * n_ceil, 0);
 
         for (int i = 0; i < dim_m; i++) {
             for (int j = 0; j < dim_n; j++) {
                 for (int x = 0; x < dim_k; x++) {
-                    C[i * dim_n + j] += A[i * dim_k + x] * B[j * dim_k + x];
+                    C[i * n_ceil + j] += A[i * k_ceil + x] * B[j * k_ceil + x];
                 }
             }
         }
@@ -263,16 +267,16 @@ int main(int argc, char *argv[]) {
         MPI_Comm row_col_comm; // ten sam row i col
         MPI_Comm_split(active, row * Pn + col, slice, &row_col_comm);
 
-        std::vector<double> R(dim_m * dim_n, 0);
+        std::vector<double> R(m_ceil * n_ceil, 0);
 
         // for (int i = 0; i < dim_m; i++) {
         //     for (int j = 0; j < dim_n; j++) {        
-        //         MPI_Reduce(&C[i * dim_n + j], &R[i * dim_n + j], 1, MPI_DOUBLE, MPI_SUM, 0,
+        //         MPI_Reduce(&C[i * n_ceil + j], &R[i * n_ceil + j], 1, MPI_DOUBLE, MPI_SUM, 0,
         //         row_col_comm);
         //     }
         // }
 
-        MPI_Reduce(&C[0], &R[0], dim_n * dim_m, MPI_DOUBLE, MPI_SUM, 0,
+        MPI_Reduce(&C[0], &R[0], n_ceil * m_ceil, MPI_DOUBLE, MPI_SUM, 0,
                 row_col_comm);
 
         // if (rank == 0) {
@@ -280,15 +284,166 @@ int main(int argc, char *argv[]) {
         //     std::cout << dim_k << " " << dim_m << " " << dim_n << "?\n";
         // }
 
-        if (slice == 0 && row == 0 && col == 0) {
-            for (int i = 0; i < dim_m; i++) {
-                for (int j = 0; j < dim_n; j++) {
-                    std::cout << R[i * dim_n + j] << " ";
-                }
-                std::cout << "\n";
-            }
-        }
+        // if (slice == 0 && row == 0 && col == 0) {
+        //     for (int i = 0; i < dim_m; i++) {
+        //         for (int j = 0; j < dim_n; j++) {
+        //             std::cout << R[i * n_ceil + j] << " ";
+        //         }
+        //         std::cout << "\n";
+        //     }
+        // }
 
+        // if (slice == 0 && col == 0) std::cout << rank << ", ";
+
+        // if (slice == 0) {
+
+        //     std::vector<double> buf(n_ceil * Pn, 0);
+        //     for (int i = 0; i < m_ceil; i++) {
+        //         MPI_Gather(
+        //             &R[i * n_ceil],
+        //             n_ceil,
+        //             MPI_DOUBLE,
+        //             &buf[0],
+        //             n_ceil,
+        //             MPI_DOUBLE,
+        //             0,
+        //             row_slice_comm
+        //         );
+
+        //         if (row == 0 && col == 0) {
+
+        //             for (int proc_col = 0; proc_col < Pn; proc_col++) {
+        //                 for (int i = proc_col * n_ceil; i < proc_col * n_ceil + (proc_col < n % Pn ? n_ceil : (n / Pn)); i++) {
+        //                     std::cout << buf[i] << " ";
+        //                 }
+        //             }
+        //             std::cout << "\n";
+
+        //             // Z zerami
+        //             // for (int i = 0; i < n_ceil * Pn; i++) {
+        //             //     std::cout << buf[i] << " ";
+        //             // }
+        //             // std::cout << "\n";
+
+        //             for (int proc_row = 1; proc_row < Pm; proc_row++) {
+        //                 int proc_rank = proc_row * Pn;
+        //                 // std::cout << proc_rank << ", ";
+        //                 MPI_Recv(
+        //                     &buf[0], /* where the message will be saved */
+        //                     n_ceil * Pn, /* max number of elements we expect */
+        //                     MPI_DOUBLE, /* type of data in the message */
+        //                     proc_rank, /* if not MPI_ANY_SOURCE, receive only from source with the given rank  */
+        //                     MPI_ANY_TAG, /* if not MPI_ANY_TAG, receive only with a certain tag */
+        //                     active, /* communicator to use */
+        //                     MPI_STATUS_IGNORE /* if not MPI_STATUS_IGNORE, write comm info here */
+        //                 );
+
+        //                 for (int proc_col = 0; proc_col < Pn; proc_col++) {
+        //                     for (int i = proc_col * n_ceil; i < proc_col * n_ceil + (proc_col < n % Pn ? n_ceil : (n / Pn)); i++) {
+        //                         std::cout << buf[i] << " ";
+        //                     }
+        //                 }
+        //                 std::cout << "\n";
+
+        //                 // Z zerami
+        //                 // for (int i = 0; i < n_ceil * Pn; i++) {
+        //                 //     std::cout << buf[i] << " ";
+        //                 // }
+        //                 // std::cout << "\n";
+        //             }                 
+        //         }
+                
+        //         else if (col == 0) {
+        //             // std::cout << rank;
+        //             MPI_Send(
+        //                 &buf[0],  /* pointer to the message */
+        //                 n_ceil * Pn, /* number of items in the message */
+        //                 MPI_DOUBLE, /* type of data in the message */
+        //                 0, /* rank of the destination process */
+        //                 0, /* app-defined message type */
+        //                 active /* communicator to use */
+        //             );
+        //         }
+
+        //     }
+        // }
+
+        if (slice == 0) {
+            int next_row = 0;
+            int print = 0;
+
+            while (next_row < dim_m) {
+                if (col == 0 && row == 0 && next_row == 0) {
+                    //std::cout << "zaczynam\n";
+                }
+                else {
+                    MPI_Recv(
+                        &print,
+                        1,
+                        MPI_INTEGER,
+                        MPI_ANY_SOURCE,
+                        MPI_ANY_TAG, /* if not MPI_ANY_TAG, receive only with a certain tag */
+                        active, /* communicator to use */
+                        MPI_STATUS_IGNORE /* if not MPI_STATUS_IGNORE, write comm info here */
+                    );
+                    //std::cout << "dostaje\n";                    
+                }
+                if (Pn == 1) {
+                    while (next_row < dim_m) {
+                        for (int i = 0; i < dim_n; i++) {
+                            std::cout << R[next_row * n_ceil + i] << " ";
+                        }
+                        next_row++; 
+                        std::cout << "\n";             
+                    }
+                    if (rank + Pn < Pn * Pm) {
+                        MPI_Send(
+                            &print,
+                            1,
+                            MPI_INTEGER,
+                            rank + Pn,
+                            0,
+                            active
+                        );                        
+                    }
+                }
+                else {
+                    for (int i = 0; i < dim_n; i++) {
+                        std::cout << R[next_row * n_ceil + i] << " ";
+                    }
+                    next_row++;
+
+                    if (col == Pn - 1) std::cout << "\n";
+
+                    int dest;
+                    // if (rank + 1 < (rank / Pn + 1) * Pn) {
+                    //     dest = rank + 1;
+                    // }
+                    // else {
+                    //     dest = (rank / Pn) * Pn;
+                    // }
+                    if (rank + 1 < (rank / Pn + 1) * Pn || next_row == dim_m) {
+                        dest = rank + 1;
+                    }
+                    else {
+                        dest = (rank / Pn) * Pn;
+                    }
+
+                    //std::cout << "wysylam\n";
+                    if (dest < Pn * Pm)
+                    MPI_Send(
+                        &print,
+                        1,
+                        MPI_INTEGER,
+                        dest,
+                        0,
+                        active
+                    );
+                    //std::cout << "wyslane\n";
+                }
+            }
+
+        }
 
     } 
 
